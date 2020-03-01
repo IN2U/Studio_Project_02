de531908390f99a1667b17d4fb4a51e99a25ee94
JunSiangLai <192792F@mymail.nyp.edu.sg>
2020-03-01 21:26:08 +0800

    Fixed - Master

---
+++ DM2122 Prac/Source/raceInitLightSettings.cpp
@@ -0,0 +1,29 @@
+#include "race.h"
+#include "../../glew/include/GL/glew.h"
+
+void SceneRace::InitLightSettings()
+{
+	light[0].type = Light::LIGHT_POINT;
+	light[0].position.Set(0, 5, 0);
+	light[0].color.Set(0.5f, 0.5f, 0.5f);
+	light[0].power = 1;
+	light[0].kC = 1.f;
+	light[0].kL = 0.01f;
+	light[0].kQ = 0.001f;
+	light[0].cosCutoff = cos(Math::DegreeToRadian(45));
+	light[0].cosInner = cos(Math::DegreeToRadian(30));
+	light[0].exponent = 3.f;
+	light[0].spotDirection.Set(0.f, 1.f, 0.f);
+
+	glUniform1i(m_parameters[U_LIGHT0_TYPE], light[0].type);
+	glUniform3fv(m_parameters[U_LIGHT0_COLOR], 1, &light[0].color.r);
+	glUniform1f(m_parameters[U_LIGHT0_POWER], light[0].power);
+	glUniform1f(m_parameters[U_LIGHT0_KC], light[0].kC);
+	glUniform1f(m_parameters[U_LIGHT0_KL], light[0].kL);
+	glUniform1f(m_parameters[U_LIGHT0_KQ], light[0].kQ);
+	glUniform3fv(m_parameters[U_LIGHT0_SPOTDIRECTION], 1, &light[0].spotDirection.x);
+	glUniform1f(m_parameters[U_LIGHT0_COSCUTOFF], light[0].cosCutoff);
+	glUniform1f(m_parameters[U_LIGHT0_COSINNER], light[0].cosInner);
+	glUniform1f(m_parameters[U_LIGHT0_EXPONENT], light[0].exponent);
+	glUniform1i(m_parameters[U_NUMLIGHTS], 1);
+}
